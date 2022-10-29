e272ac65f7005c9797f5da42a2526b455820bf60
Octopusman388 <d3adlol0@gmail.com>
2022-10-29 16:27:04 +0200

    Update getDatasFromBinance_public.py
    Updated the file so you don't need to edit the code

--- getDatasFromBinance_public.py
+++ getDatasFromBinance_public.py
@@ -1,14 +1,16 @@
 from binance.client import Client
 import json
 
-api_key = '{YOUR_API_KEY}'
-api_secret = '{YOUR_API_SECRET}'
-client = Client(api_key, api_secret)
-symbol = '{SYMBOL}' # example: 'BNBUSDT'
-print("downloading data from Binance for symbol: " + symbol)
-klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_5MINUTE,"30 Dec, 2017", "1 Jan, 2020")
+client = Client()
+nameOutputFile = input("Enter the name of the output file without extension :")
+symbol = input("Enter the symbol (ex: BTCUSDT) : ")
+interval = input("Enter the interval (ex: 1m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M) :")
+startDate = input("Enter the start date (ex: 1 Jan, 2020) :")
+endDate = input("Enter the end date (ex: 1 Jan, 2021) : ")
+klines = client.get_historical_klines(symbol, interval, startDate, endDate)
 
 # export the klines to a json file
-with open('dataBinance_2018-2019_5m_unformated.json', 'w') as outfile:
+with open(str(nameOutputFile)+'.json', 'w') as outfile:
     json.dump(klines, outfile)
+    print("Json file created")
 
